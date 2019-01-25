dd672555f17cf3b0b0e30ec3a84074e30d275a91
prajjwal <prajjyadav0904@gmail.com>
2019-01-26 04:50:20 +0530

    ui properties added

--- app/src/main/java/com/example/prashanjeet/firebase/MainActivity.java
+++ app/src/main/java/com/example/prashanjeet/firebase/MainActivity.java
@@ -9,36 +9,35 @@ import android.widget.Button;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 public class MainActivity extends AppCompatActivity {
-
-    DatabaseReference myRef;
-    Button btn;
-
-    public void btnClicked(View v){
-        startActivity(new Intent(MainActivity.this,LiveQueue.class));
-    }
-
+    private Button gotoLogin,gotoAdminLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-
-        myRef = database.getReference("message");
-        myRef.setValue("Hello, World!");
-        btn = (Button)findViewById(R.id.btn);
-
-       // startActivity(new Intent(MainActivity.this,LiveQueue.class));
+        gotoLogin=(Button)findViewById(R.id.GotouserLogin);
+        gotoAdminLogin=(Button)findViewById(R.id.GotoAdminLogin);
+        gotoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent  intent  = new  Intent(MainActivity.this,UserLogin.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        gotoAdminLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent  intent  = new  Intent(MainActivity.this,UserSignup.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-    }
-
 }
